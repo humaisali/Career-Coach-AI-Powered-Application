43bd40fa7918003995d15d978c956c846194ce32
Humais Ali <humaisali.uetm282@gmail.com>
2026-05-06 13:53:03 +0500

    Update the server URL
    Add the deployed Server URL

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://career-coach-ohde.onrender.com',
         changeOrigin: true,
       },
     },
